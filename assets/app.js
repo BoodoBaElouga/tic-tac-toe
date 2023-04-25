444c772d1fc79cb97f3eac3a9250fd0138398b2e
pavel <pavel_boodo@yahoo.com>
2023-04-25 21:31:13 +0200

    jquery and first javascript function (eventlistener for play button) were added

--- assets/app.js
+++ assets/app.js
@@ -11,4 +11,46 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
-console.log("Ich bin hier");
+// import jquery
+import $ from 'jquery';
+
+$(document).ready(function () {
+  $('#play').click(function () {
+    $.ajax({
+      url: "/play",
+      type: "POST",
+      data: { /* optional: Daten, die an den Controller übergeben werden sollen */},
+      success: function (response) {
+        // Handle die Antwort vom Controller
+        $('body').html(response);
+
+        history.pushState(null, null, "/play");
+      },
+      error: function (xhr) {
+        // Handle die Fehlermeldung, falls der Aufruf fehlschlägt
+        console.log(xhr.responseText);
+      }
+    });
+  });
+
+
+  $('#replay').click(function () {
+    $.ajax({
+      url: "/",
+      type: "POST",
+      data: { /* optional: Daten, die an den Controller übergeben werden sollen */},
+      success: function (response) {
+        // Handle die Antwort vom Controller
+        $('body').html(response);
+
+        history.pushState(null, null, "/play");
+      },
+      error: function (xhr) {
+        // Handle die Fehlermeldung, falls der Aufruf fehlschlägt
+        console.log(xhr.responseText);
+      }
+    });
+  });
+
+});
+
